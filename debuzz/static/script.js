d55d0e0cf9b402fa3ebbf286ac6b5c3e16c48e5c
a-nosnitram <stasymartinson@gmail.com>
2025-03-16 06:56:37 +0000

    index update

---
+++ debuzz/static/script.js
@@ -0,0 +1,45 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();  // Prevents default jump
+        const target = document.querySelector(this.getAttribute('href'));
+
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
+
+async function debuzz() {
+    console.log('Debuzzing...');
+
+    const textarea = document.getElementById('buzzwordInput');
+    const text = textarea.value;
+
+    try {
+        const response = await fetch('http://localhost:7777/api/debuzz', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify([text]),
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.error || 'Unknown error occurred.');
+        }
+
+        const result = await response.json();
+        if (!Array.isArray(result) || result.length === 0) {
+            throw new Error("Invalid response format from the server.");
+        }
+
+        textarea.value = result[0] || 'No output received';
+
+
+    } catch (error) {
+        console.error('Debuzzing error:', error.message);
+        alert(`Error: ${error.message}`);
+    }
+}
